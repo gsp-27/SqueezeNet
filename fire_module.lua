47cbdbb63434839e8c031168c9f196655f236201
Gaurav Pathak <gspat27@gmail.com>
2016-11-10 14:31:26 +0530

    Replicated the squeeze net architechture of the paper

--- fire_module.lua
+++ fire_module.lua
@@ -16,7 +16,43 @@ local function FireModule(nInputPlane, s, e1, e2)
 	return sNet
 end
 
-local model = FireModule(3, 5, 4, 6)
-print(model)
-input = torch.randn(10,3,7,7)
-print(model:forward(input))
+-- here will construct the entire squeezenet model as mentioned in the paper
+local model = nn.Sequential()
+-- first convolution layer output will be (96,16,16) 
+model:add(nn.SpatialConvolution(3,96,3,3,1,1,1,1))
+model:add(nn.ReLU(true))
+model:add(nn.SpatialMaxPooling(2,2,2,2))
+
+-- now have to add 8 fire modules
+s1x1 = {16,16,32,32,48,48,64,64}
+e1x1 = {64,64,128,128,192,192,256,256}
+e3x3 = {64,64,128,128,192,192,256,256}
+
+-- fire module 1 output (128,16,16)
+model:add(FireModule(96,s1x1[1],e1x1[1],e3x3[1]))
+-- fire module 2 output (128,16,16)
+model:add(FireModule(128,s1x1[2],e1x1[2],e3x3[2]))
+-- fire module 3 output (256,16,16)
+model:add(FireModule(128,s1x1[3],e1x1[3],e3x3[3]))
+model:add(nn.SpatialMaxPooling(2,2,2,2)) -- input size is reduced by 2, so (256,8,8)
+-- fire module 4 output (256,8,8)
+model:add(FireModule(256,s1x1[4],e1x1[4],e3x3[4]))
+-- fire module 5 output (384,8,8)
+model:add(FireModule(256,s1x1[5],e1x1[5],e3x3[5]))
+-- fire module 6 output (384,8,8)
+model:add(FireModule(384,s1x1[6],e1x1[6],e3x3[6]))
+-- fire module 7 output (512,8,8)
+model:add(FireModule(384,s1x1[7],e1x1[7],e3x3[7]))
+model:add(nn.SpatialMaxPooling(2,2,2,2)) -- size reduced by 2 so (512,4,4)
+-- fire module 8 output (512,4,4)
+model:add(FireModule(512,s1x1[8],e1x1[8],e3x3[8]))
+model:add(nn.SpatialConvolution(512,10,1,1))
+model:add(nn.ReLU(true))
+model:add(nn.SpatialAveragePooling(4,4,1,1))
+model:add(nn.Reshape(10, true))
+return model
+
+-- print(model)
+-- input = torch.randn(20,3,32,32)
+-- scores = model:forward(input)
+-- print(scores:size())
